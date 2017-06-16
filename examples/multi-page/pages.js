0433681c40323c4c55a6511f3341210453a14572
Klāvs Priedītis <klavs.pr@gmail.com>
2017-06-17 01:15:35 +0300

    implement helper to generate page link

--- examples/multi-page/pages.js
+++ examples/multi-page/pages.js
@@ -3,3 +3,5 @@ export const CUSTOMER_INFO_PAGE = "customer-info"
 export const CART_PAGE = "cart"
 export const DELIVERY_PAGE = "delivery"
 export const BILLING_PAGE = "billing"
+
+export default page => `/multi-page/${page}/`
